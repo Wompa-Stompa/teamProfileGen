908f40e9d57cb11d402ae132d36a96c574fc2f52
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-08-13 12:32:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,177 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/manager.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+const Employee = require('./lib/employee.js');
+const fs = require('fs');
+
+var employees = [];
+
+const managerProfile = function () {
+   return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: "Team manager's name (required)",
+            validate: name => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log('Please enter a name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'Enter employee ID (required)',
+            validate: id => {
+                if (id) {
+                    return true;
+                } else {
+                    console.log('Please enter an ID');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "Employee's Email (required)",
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Please enter an Email');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'office',
+            message: 'Enter employee office number (required)',
+            validate: office => {
+                if (office) {
+                    return true;
+                } else {
+                    console.log('Please enter an office number');
+                    return false;
+                }
+            }
+        },
+   ])
+       
+    .then(managerData => {
+        const { name, id, email, officeNumber } = managerData;
+        const manager = new Manager(name, id, email, officeNumber);
+        employees.push(manager);
+    })
+};
+
+
+const menu = function () {
+  return  inquirer.prompt([
+        {
+        type: 'list',
+        message: 'Add an employee',
+        name: 'role',
+        choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'text',
+            name: 'name',
+            message: 'Employee name',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'Enter employee ID',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an ID');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'Enter employee email address',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'github',
+            message: 'Enter employee Github',
+            when: (input) => input.role === 'Engineer',
+            validate: github => {
+                if (github) {
+                    return true;
+                } else {
+                    console.log('Please enter a github username');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'school',
+            message: "Enter intern's school",
+            when: (input) => input.role === 'Intern',
+            validate: school => {
+                if (school) {
+                    return true;
+                } else {
+                    console.log('Please enter a school');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add another employee?',
+            default: false
+        }
+
+    ])
+    .then(employeesData => {
+        let { name , id, email, role, github, school, confirmAddEmployee} = employeesData;
+        let employee;
+
+        if(role === 'Engineer') {
+            employee = new Engineer(name, id, email, github);
+            console.log(employee);
+        } else if (role === "Intern") {
+            employee = new Intern(name, id, email, school);
+            console.log(employee);
+        }
+        employees.push(employee);
+
+        if(confirmAddEmployee) {
+            return menu(employees)
+        } else {
+            return employees;  
+        }
+    })
+    
+};
